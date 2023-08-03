30c542c7e27dd2f106b7f2dc20ed53b0f4f46268
Getayawkal Wondimagegnehu <Getayawkal.won@gmail.com>
2023-08-03 13:34:21 -0700

    Recursion

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+int actual_sqrt_recursion(int n, int i);
+
+/**
+ * Return: the resulting square root
+ */
+int _sqrt_recursion(int n)
+{
+	if (n < 0)
+		return (-1);
+	return (actual_sqrt_recursion(n, 0));
+}
+
+/**
+ * Return: the resulting square root
+ */
+int actual_sqrt_recursion(int n, int i)
+{
+	if (i * i > n)
+		return (-1);
+	if (i * i == n)
+		return (i);
+	return (actual_sqrt_recursion(n, i + 1));
+}
+
