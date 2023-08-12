1708206d8f7d6f677f76e6b368089c635c4953b6
Xtremehonor <104856930+Xtremehonor@users.noreply.github.com>
2023-08-12 18:36:34 +0300

    UPDATING 100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -4,25 +4,24 @@
  * main - prints all possible different combinations of two digits
  * Return: ALways 0 (Success)
  */
-int main(void) 
-{
-int num;
-int numb;
+int main() {
+    int num;
+    int numb;
 
-for (num = 48;num <= 57;num++)
-    for(numb = 48;numb <= 57;numb++)
-    {
-        if (num < numb)
+    for (num=48;num <= 57;num++)
+        for(numb=48;numb <= 57;numb++)
         {
-        putchar(num);
-        putchar(numb);
-            if (num != 56 || numb != 57)
+            if (num < numb)
+            {
+            putchar(num);
+            putchar(numb);
+            if (num!=56 || numb!=57)
             {
                putchar(',');
                putchar(' ');
             }
             }
-    }
+            }
     putchar('\n');
     return (0);
 }
